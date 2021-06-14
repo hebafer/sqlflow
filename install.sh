9788be447730ffe46b37775cfccd3d8382f0bb42
hebafer <hectorballega@gmail.com>
2021-06-14 20:58:16 +0200

    Modify prometheus to collect metrics from argo

--- install.sh
+++ install.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
 # Delete Argo namespace and running PODs on it
-helm uninstall argo-workflows
 kubectl delete all --all -n argo
 kubectl delete namespaces argo
 # Terminate process running on Argo & SQLFlow Notebook
-fuser -k 9001/tcp && fuser -k 8000/tcp
+fuser -k 50051/tcp && fuser -k 9001/tcp && fuser -k 8000/tcp
 
 # Install Argo
 kubectl create namespace argo
 kubectl create rolebinding default --clusterrole=admin --serviceaccount=default:default
 kubectl create rolebinding sqlflow --clusterrole=admin --serviceaccount=default:sqlflow --namespace=default
 kubectl create configmap -n argo workflow-controller-configmap --from-literal=config="containerRuntimeExecutor: pns"
-helm install argo-workflows --namespace argo argo/argo-workflows
+kubectl apply -n argo -f https://raw.githubusercontent.com/argoproj/argo/v2.12.11/manifests/install.yaml
+
 # Expose Argo workflow
 while [ "$(kubectl get pods -n argo -o jsonpath='{.items[0].status.containerStatuses[*].ready}')" != "true" ]; do
    sleep 5
    echo "Waiting for Argo to be ready..."
 done
 # Port Forward argo-server port
+kubectl create role prometheus-k8s --namespace argo --resource services,endpoints,pods --verb get,list,watch
+kubectl create rolebinding prometheus-k8s --namespace argo --role prometheus-k8s --serviceaccount stable-kube-prometheus-sta-prometheus
+kubectl label service argo-workflows-server app=workflow-controller --namespace argo
 nohup kubectl -n argo port-forward svc/argo-workflows-server 9001:2746 --address=0.0.0.0 &
 
-# Install chart
+# Install SQLFlow chart
 kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
 helm uninstall sqlflow
 helm install sqlflow charts/sqlflow/
@@ -31,13 +34,6 @@ nohup kubectl port-forward deployment/sqlflow 50051:50051 --address=0.0.0.0 &
 kubectl get pods --field-selector=status.phase!=Running -o name | xargs kubectl delete
 
 # Install Grafana and Prometheus to collect metrics
-helm repo add stable https://charts.helm.sh/stable
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-
-kubectl create role prometheus-k8s --namespace argo --resource services,endpoints,pods --verb get,list,watch
-kubectl create rolebinding prometheus-k8s --namespace argo --role prometheus-k8s --serviceaccount stable-kube-prometheus-sta-prometheus
-#kubectl label service argo-workflows-server app=workflow-controller --namespace argo
-
-helm install stable prometheus-community/kube-prometheus-stack
-
+helm install prometheus-stack prometheus-community/kube-prometheus-stack --namespace monitor -f prometheus/values.yaml
 nohup kubectl port-forward service/stable-grafana 8888:80 &
